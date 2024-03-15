5b78e1c186ddcbd96130dc7d8316e9d74345e7d2
Luiz Felipe <70807899+elfihr@users.noreply.github.com>
2024-03-14 22:04:19 -0300

    aula134

---
+++ secao10/aula133-ViewsExpress/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({extended:true}))
+//express caminho pasta views
+app.set('views', path.resolve(__dirname,'src','views'))
+app.set('view engine', 'ejs')// << ejs para renderizar HTML 
+//fazer expresse usar o route
+app.use(routes)
+
+// app.get('/teste',(req,res) //com essa informação voce consegue acessar a rota testes
+app.get('/teste/:idUsuarios?',(req,res) =>{ //usandoidusuarios ele nao acessa a rota testes sem acrescentar /qualquerCoisa use sempre ? apos o final
+    //que significa que ele pode ou nao receber o informação apos rota
+    console.log(req.params)
+    console.log(req.query)
+    res.send('oi')//mensagem apos acessar rota
+})
+
+//é nescessario fazer o tratamento do post atraves do app.use(express.urlencoded({extended:true}))
+//Usado para formularios
+app.post('/',(req,res) => {
+    console.log(req.body)
+    res.send(`Captei vossa menagem!! MENSAGEM: ${req.body.conteudo}`)
+})
+
+app.get('/contato',(req,res) => {
+    res.send('Thank you')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Iniciando Server em http://localhost:3000')
+})
---
+++ secao10/aula134-Arquivoestatico/src/controllers/homeController.js
@@ -0,0 +1,7 @@
+exports.paginaIncial = (req,res) => {
+    res.send(index)
+}
+
+exports.trataPost = (req,res) =>{
+    res.send('nova rota')
+}
